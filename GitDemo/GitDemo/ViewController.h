f8ee4bc58d92afdd6e1eae0bd376dba747248074
Json2017 <316125818@qq.com>
2016-12-26 12:50:04 +0800

    Add files via upload

---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Json on 2016/12/26.
+//  Copyright © 2016年 Json. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(void)sayHello;
+-(void)sayBye;
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by Json on 2016/12/26.
+//  Copyright © 2016年 Json. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self sayHello];
+    
+    [self sayBye];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void)sayHello{
+    NSLog(@"Hello");
+}
+-(void)sayBye{
+    NSLog(@"Bye");
+}
+@end
